05209c33b63f058bca2c448637f96248674343f9
afelipevargas <devafvargas@gmail.com>
2021-05-05 01:17:04 +0000

    TASK10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * check_cycle - Checks if there is a loop in a linked list.
+ * @list: pointer to linked list.
+ * Return: returns 1 if there is a loop and 0 if there isn't.
+ */
+int check_cycle(listint_t *list)
+{
+    if (list != NULL)
+    {
+        listint_t *head = NULL;
+        listint_t *aux = NULL;
+        head = list;
+        aux = head;
+        while (aux)
+        {
+            if (aux->next == NULL)
+            {
+                return (0);
+            }
+            aux = aux->next;
+            if (aux == head)
+            {
+                return (1);
+            }
+        }
+    }
+    return (0);
+}
