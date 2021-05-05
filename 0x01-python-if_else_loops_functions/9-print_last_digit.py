61916e07170fa935d41f3f4f63d7871891c8855d
afelipevargas <devafvargas@gmail.com>
2021-05-05 21:03:34 +0000

    TASK9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        digit = (number % -10)
+        print(digit, end='')
+        return digit
+    elif number > 0:
+        digit = (number % 10)
+        print(digit, end='')
+        return digit
+    else:
+        print(0, end='')
+        return 0
