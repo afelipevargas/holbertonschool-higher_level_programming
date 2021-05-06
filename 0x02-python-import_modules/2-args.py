4f33bc0346749b5f641186b301aa1e9b95162f3f
afelipevargas <devafvargas@gmail.com>
2021-05-06 15:41:27 +0000

    TASK2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,11 @@
 if __name__ == "__main__":
     import sys
     size = (len(sys.argv) - 1)
-    if (size == 0):
+    if size == 0:
         print('0 arguments.')
+    elif size == 1:
+        print('{:d} argument:'.format(size))
+        print('{:d}: {:s}'.format(size, sys.argv[size]))
     else:
         index = 1
         print('{:d} arguments:'.format(size))
