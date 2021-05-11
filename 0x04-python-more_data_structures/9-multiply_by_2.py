27e049bc25f88265ee506458b7e1e8b47157577f
afelipevargas <devafvargas@gmail.com>
2021-05-11 19:05:05 +0000

    TASK9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dic = a_dictionary.copy()
-    
+
     for key, value in new_dic.items():
         tmp = value
         new_dic[key] = tmp * 2
